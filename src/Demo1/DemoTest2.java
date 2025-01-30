51eebc826e83cccca37d22292b1fe3e09819cfc3
sayed <sayed@Sameer.mynetworksettings.com>
2025-01-29 21:14:11 -0500

    age and name added

--- src/Demo1/DemoTest2.java
+++ src/Demo1/DemoTest2.java
@@ -3,6 +3,11 @@ package Demo1;
 public class DemoTest2 {
 
 	public static void main(String[]args) {
-		System.out.println("Java");
+		
+		String name = "John"; 
+        int age = 25;        
+
+        System.out.println("Hello, " + name + "!");
+        System.out.println("You are " + age + " years old.");
 	}
 }
